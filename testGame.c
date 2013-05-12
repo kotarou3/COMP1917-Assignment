f5b40c1746816f60bf128fab970669ed94b64c24
小太 <nospam.kotarou.dono@gmail.com>
2013-05-12 12:47:25 +1000

    Add exchange rate tests

--- testGame.c
+++ testGame.c
@@ -3,6 +3,11 @@
 
 #include "test-utils.h"
 
+const int allUnis[] = {UNI_A, UNI_B, UNI_C};
+const int uniCount = sizeof(allUnis) / sizeof(allUnis[0]);
+const int allDegrees[] = {STUDENT_THD, STUDENT_BPS, STUDENT_BQN, STUDENT_MJ, STUDENT_MTV, STUDENT_MMONEY};
+const int degreeCount = sizeof(allDegrees) / sizeof(allDegrees[0]);
+
 const int testDegreeValues[] = TEST_DEGREE_VALUES;
 const int testDiceValues[] = TEST_DICE_VALUES;
 const region initOrder[] = {{-2,0},{-2,1},{-2,2},{-1,-1},{-1,0},{-1,1},{-1,2},{0,-2},{0,-1},{0,0},{0,1},{0,2},{1,-2},{1,-1},{1,0},{1,1},{2,-2},{2,-1},{2,0}};
@@ -21,22 +26,46 @@ bool testGameCreation() {
 
     // Test universities
     int u = 0;
-    while (u < NUM_UNIS) {
-        fail_str(getARCs(g, u) == 0, "getARCs(g, %d) == 0", u);
-        fail_str(getCampuses(g, u) == 2, "getCampuses(g, %d) == 2", u);
-        fail_str(getGO8s(g, u) == 0, "getGO8s(g, %d) == 0", u);
+    while (u < uniCount) {
+        int playerId = allUnis[u];
 
-        fail_str(getPublications(g, u) == 0, "getPublications(g, %d) == 0", u);
-        fail_str(getIPs(g, u) == 0, "getIPs(g, %d) == 0", u);
+        fail_str(getARCs(g, playerId) == 0, "getARCs(g, %d) == 0", playerId);
+        fail_str(getCampuses(g, playerId) == 2, "getCampuses(g, %d) == 2", playerId);
+        fail_str(getGO8s(g, playerId) == 0, "getGO8s(g, %d) == 0", playerId);
 
-        fail_str(getStudents(g, u, STUDENT_THD) == 0, "getStudents(g, %d, STUDENT_THD) == 0", u);
-        fail_str(getStudents(g, u, STUDENT_BPS) == 3, "getStudents(g, %d, STUDENT_BPS) == 3", u);
-        fail_str(getStudents(g, u, STUDENT_BQN) == 3, "getStudents(g, %d, STUDENT_BQN) == 3", u);
-        fail_str(getStudents(g, u, STUDENT_MJ) == 1, "getStudents(g, %d, STUDENT_MJ) == 1", u);
-        fail_str(getStudents(g, u, STUDENT_MTV) == 1, "getStudents(g, %d, STUDENT_MTV) == 1", u);
-        fail_str(getStudents(g, u, STUDENT_MMONEY) == 1, "getStudents(g, %d, STUDENT_MMONEY) == 1", u);
+        fail_str(getPublications(g, playerId) == 0, "getPublications(g, %d) == 0", playerId);
+        fail_str(getIPs(g, playerId) == 0, "getIPs(g, %d) == 0", playerId);
 
-        // TODO: exchange rates
+        fail_str(getStudents(g, playerId, STUDENT_THD) == 0, "getStudents(g, %d, STUDENT_THD) == 0", playerId);
+        fail_str(getStudents(g, playerId, STUDENT_BPS) == 3, "getStudents(g, %d, STUDENT_BPS) == 3", playerId);
+        fail_str(getStudents(g, playerId, STUDENT_BQN) == 3, "getStudents(g, %d, STUDENT_BQN) == 3", playerId);
+        fail_str(getStudents(g, playerId, STUDENT_MJ) == 1, "getStudents(g, %d, STUDENT_MJ) == 1", playerId);
+        fail_str(getStudents(g, playerId, STUDENT_MTV) == 1, "getStudents(g, %d, STUDENT_MTV) == 1", playerId);
+        fail_str(getStudents(g, playerId, STUDENT_MMONEY) == 1, "getStudents(g, %d, STUDENT_MMONEY) == 1", playerId);
+
+        int d = 0;
+        while (d < degreeCount) {
+            int degreeFromType = allDegrees[d];
+            if (degreeFromType != STUDENT_THD) {
+                int d2 = 0;
+                while (d2 < degreeCount) {
+                    int degreeToType = allDegrees[d2];
+
+                    int correctExchangeRate = 3;
+                    if ((playerId == UNI_A && degreeToType == STUDENT_MTV) ||
+                        (playerId == UNI_B && degreeToType == STUDENT_BQN) ||
+                        (playerId == UNI_C && degreeToType == STUDENT_BPS))
+                        correctExchangeRate = 2;
+
+                    fail_str(getExchangeRate(g, playerId, degreeFromType, degreeToType) == correctExchangeRate,
+                        "getExchangeRate(g, %d, %d, %d) == %d", playerId, degreeFromType, degreeToType, correctExchangeRate);
+
+                    ++d2;
+                }
+            }
+            ++d;
+        }
+
         ++u;
     }
 
