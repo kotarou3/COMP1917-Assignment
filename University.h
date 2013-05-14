e9e88cc3a34eec68f2747120e7a457aea7ea4467
小太 <nospam@xiaotai.org>
2013-05-14 19:29:30 +1000

    Revert "Put getCampuses, getARCs and getGO8s into functions getCampsCount, etc. and updated all instances where these were used"
    This reverts commit c21eed250847ae7e982e0427a09ce4280b8cb613.

--- University.h
+++ University.h
@@ -59,12 +59,8 @@ int getIPs(Game* game, PlayerId player);
 void constructUniversity(University* university, PlayerId player);
 void destroyUniversity(University* university);
 
-//better named functions than those in Game.h
-//Called from those in Game.h
-int getARCCount(Game* game, PlayerId player);
-int getCampusCount(Game* game, PlayerId player);
-int getGO8Count(Game* game, PlayerId player);
-
+int getNormalCampusCount(const University* university);
+int getGo8CampusCount(const University* university);
 int getStudentExchangeRate(const University* university, DegreeType from, DegreeType to);
 
 bool isPossibleAction(University* university, Map* map, Action action);
